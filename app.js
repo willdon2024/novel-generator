d66bd41c178c9381a7aadbf92b502d4fa43b71b3
will-1985 <sowell8@foxmail.com>
2025-01-12 17:09:23 +0800

    Update: Fix auth validation and code structure

---
+++ app.js
@@ -0,0 +1,77 @@
+// 进度管理器类
+class ProgressManager {
+    constructor() {
+        this.currentStep = 1;
+        this.totalSteps = 5;
+        this.progress = {};
+    }
+    
+    updateProgress(step, data) {
+        this.progress[step] = data;
+    }
+    
+    getProgress(step) {
+        return this.progress[step];
+    }
+    
+    nextStep() {
+        if (this.currentStep < this.totalSteps) {
+            this.currentStep++;
+            return true;
+        }
+        return false;
+    }
+    
+    prevStep() {
+        if (this.currentStep > 1) {
+            this.currentStep--;
+            return true;
+        }
+        return false;
+    }
+    
+    getCurrentStep() {
+        return this.currentStep;
+    }
+    
+    reset() {
+        this.currentStep = 1;
+        this.progress = {};
+    }
+}
+
+// 应用状态管理类
+class AppState {
+    constructor() {
+        this.apiKey = null;
+        this.currentNovel = null;
+        this.progressManager = new ProgressManager();
+    }
+    
+    setApiKey(key) {
+        this.apiKey = key;
+        localStorage.setItem('apiKey', key);
+    }
+    
+    getApiKey() {
+        return this.apiKey;
+    }
+    
+    setCurrentNovel(novel) {
+        this.currentNovel = novel;
+    }
+    
+    getCurrentNovel() {
+        return this.currentNovel;
+    }
+    
+    reset() {
+        this.apiKey = null;
+        this.currentNovel = null;
+        this.progressManager.reset();
+    }
+}
+
+// 创建单例实例
+const appState = new AppState();
+const progressManager = appState.progressManager; 
